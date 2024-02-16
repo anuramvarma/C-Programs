a8121aad3cbac70edce89fd3e6f91c727370b95a
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-02-16 21:58:59 +0530

    Add files via upload

---
+++ Smallest of Three numbers.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+int main ()
+{
+int a,b,c,d;
+printf("Enter a value of a : ");
+scanf("%d",&a);
+printf("Enter a value of b : ");
+scanf("%d",&b);
+printf("Enter a value of c : ");
+scanf("%d",&c);
+printf("Enter a value of d : ");
+scanf("%d",&d);
+if (a<b&&a<c&&a<d)
+printf("a is small");
+else if (b<a&&b<c&&b<d)
+printf("b is small");
+else if (c<a&&c<b&&c<d)
+printf("c is small");
+else 
+printf("d is small");
+return 0;
+}
