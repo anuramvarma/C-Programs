1a52856b683ac95f58a2b2c785b89eeccef1fcf7
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-02 16:41:46 +0530

    Update Electricity Bill.c

--- Electricity Bill.c
+++ Electricity Bill.c
@@ -6,11 +6,11 @@ printf("Enter the units=");
 scanf("%f",&n);
  if(n<=50)
 printf("Bill=%f",n*0.5);
-else if (n<=100)
+ else if (n<=100)
 printf("Bill=%f",25+(n-50)*0.75);
  else if (n<=200)
 printf("Bill=%f",25+37.5+(n-100)*1.2);
-else
+ else
 printf("Bill=%f",25+37.5+120+(n-200)*1.50);
  return 0;
 }
