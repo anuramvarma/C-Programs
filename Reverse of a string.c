79946f68a891af42ec8473dfa524c6479ad585eb
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 14:56:48 +0530

    Update Reverse of a string.c

--- Reverse of a string.c
+++ Reverse of a string.c
@@ -3,16 +3,16 @@
 int main()
 {
   	char Str[100], RevStr[100];
-  	int i, j, len;
-  	printf("\n Please Enter any String :  ");
+  int i, j, len;
+ 	printf("\n Please Enter any String :  ");
   	gets(Str); 	
   	j = 0;
-  	len = strlen(Str);
+ 	len = strlen(Str);
   	for (i = len - 1; i >= 0; i--)
-  	{
-  		RevStr[j++] = Str[i];
-  	}
+     	{
+  	  	RevStr[j++] = Str[i];
+    	}
   	RevStr[i] = '\0';
-  	printf("\n String after Reversing = %s", RevStr); 	
+    	printf("\n String after Reversing = %s", RevStr); 	
   	return 0;
 }
