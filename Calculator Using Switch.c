d27349401917da807a9437c493b9b19ada0a6db7
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 14:55:48 +0530

    Update Calculator Using Switch.c

--- Calculator Using Switch.c
+++ Calculator Using Switch.c
@@ -3,11 +3,11 @@ int main ()
 {
  float a,b;
  printf("a=");
-scanf("%f",&a);
+   scanf("%f",&a);
 printf("b=");
-scanf("%f",&b);
-int choice;
-scanf("%d",&choice);
+   scanf("%f",&b);
+ int choice;
+   scanf("%d",&choice);
 printf("1 addtion \n");
 printf("2 subtraction \n");
 printf("3 multiplication \n");
@@ -15,17 +15,16 @@ printf("3 multiplication \n");
  printf("choice=");
 switch(choice)
 {
-case 1:printf("%f\n",a+b);
+    case 1:printf("%f\n",a+b);
 break;
-case 2:printf("%f\n",a-b);
+    case 2:printf("%f\n",a-b);
 break;
- case 3:printf("%f\n",a*b);
+    case 3:printf("%f\n",a*b);
 break;
-case 4:printf("%f\n",a/b);
+    case 4:printf("%f\n",a/b);
 break;
 
-default:printf("enter values only from 1-4 only");
+    default:printf("enter values only from 1-4 only");
 }
-
 return 0;
 }
