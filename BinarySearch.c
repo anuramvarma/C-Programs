a46a1cb7b2d300241c8b6b251620218007348119
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 19:12:05 +0530

    Update and rename Binary.c to BinarySearch.c

--- BinarySearch.c
+++ BinarySearch.c
@@ -3,15 +3,15 @@ int main()
 {  
 int a[5]={1,2,3,4,5},low=0,high=4,i,k,f=0,mid;
 printf("enter key:");
-scanf("%d",&k);
+  scanf("%d",&k);
 while(low<=high)
    {
-mid=(low+high)/2;
+    mid=(low+high)/2;
 
-if(k==a[mid])
+    if(k==a[mid])
  {
    f=1;
-break;
+   break;
  }
 else if(k<a[mid])
  high=mid-1;
@@ -23,4 +23,3 @@ if(f==1)
 else
  printf("key not found");
 }
-      
