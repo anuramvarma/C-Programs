26baa9547bc48379bb39e45daa0f9d01ee2895b3
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 19:10:51 +0530

    Update LinearSearch.c

--- LinearSearch.c
+++ LinearSearch.c
@@ -4,21 +4,23 @@ int main()
     int i,n,key,pos=-1;
     int a[100];
     printf("THE NUMBER OF ELEMENTS:");
-    scanf("%d",&n);
-    for(i=0;i<n;i++){
-    scanf("%d",&a[i]);}
-    printf("enter the element to search");
-    scanf("%d",&key);
+       scanf("%d",&n);
     for(i=0;i<n;i++)
-    if(key==a[i])
     {
-    pos=i;
-    break;
+      scanf("%d",&a[i]);
     }
-    {
-    if(pos>=0)
-    printf("key is found=%d",pos);
-    else
-    printf("key is not found");
-    }
+      printf("enter the element to search");
+         scanf("%d",&key);
+    for(i=0;i<n;i++)
+     if(key==a[i])
+      {
+       pos=i;
+        break;
+      }
+         {
+           if(pos>=0)
+              printf("key is found=%d",pos);
+            else
+               printf("key is not found");
+         }
 }
