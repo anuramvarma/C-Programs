ab424b6a161344416ce714de1a2b34bdfdc71926
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 19:09:10 +0530

    Update Smallest of Three numbers.c

--- Smallest of Three numbers.c
+++ Smallest of Three numbers.c
@@ -2,21 +2,21 @@
 int main ()
 {
 int a,b,c,d;
-printf("Enter a value of a : ");
-scanf("%d",&a);
-printf("Enter a value of b : ");
-scanf("%d",&b);
-printf("Enter a value of c : ");
-scanf("%d",&c);
-printf("Enter a value of d : ");
-scanf("%d",&d);
+ printf("Enter a value of a : ");
+    scanf("%d",&a);
+ printf("Enter a value of b : ");
+    scanf("%d",&b);
+ printf("Enter a value of c : ");
+    scanf("%d",&c);
+ printf("Enter a value of d : ");
+    scanf("%d",&d);
 if (a<b&&a<c&&a<d)
-printf("a is small");
+   printf("a is small");
 else if (b<a&&b<c&&b<d)
-printf("b is small");
+   printf("b is small");
 else if (c<a&&c<b&&c<d)
-printf("c is small");
+   printf("c is small");
 else 
-printf("d is small");
+   printf("d is small");
 return 0;
 }
