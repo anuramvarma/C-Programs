08ce249325ed20f32b4bf9823b0217063e37cc29
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 14:50:40 +0530

    Update Roots of a quadratic Equation.c

--- Roots of a quadratic Equation.c
+++ Roots of a quadratic Equation.c
@@ -2,17 +2,17 @@
 #include<math.h>
 int main ()
 {
-int a,b,c;
-float alpha,beta;
-printf("Enter a value of a : ");
-  scanf("%d",&a);
-printf("Enter a value of b : ");
- scanf("%d",&b);
-printf("Enter a value of c :");
-scanf("%d",&c);
-alpha=(-b+sqrt(b*b-4*a*c))/2*a;
-beta=(-b-sqrt(b*b-4*a*c))/2*a;
-printf("Roots of a quadratic Equation are : ");
-printf("%f%f",alpha,beta);
-return 0;
+   int a,b,c;
+    float alpha,beta;
+      printf("Enter a value of a : ");
+        scanf("%d",&a);
+      printf("Enter a value of b : ");
+        scanf("%d",&b);
+      printf("Enter a value of c :");
+        scanf("%d",&c);
+    alpha=(-b+sqrt(b*b-4*a*c))/2*a;
+    beta=(-b-sqrt(b*b-4*a*c))/2*a;
+   printf("Roots of a quadratic Equation are : ");
+   printf("%f%f",alpha,beta);
+ return 0;
 }
