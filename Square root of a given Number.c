f766ce9c6aec1c0332ec1ca8f73073b609a3b86f
Anuram Varma Mudunuri <148671965+anuramvarma@users.noreply.github.com>
2024-03-24 14:49:20 +0530

    Update Square root of a given Number.c

--- Square root of a given Number.c
+++ Square root of a given Number.c
@@ -2,10 +2,10 @@
 #include<math.h>
 int main()
 {
-float a,b;
-printf("Enter a value ");
-scanf("%f",&a);
-b=sqrt(a);
-printf("sqrt of a given value =%f",b);
-return 0;
+   float a,b;
+  printf("Enter a value ");
+    scanf("%f",&a);
+  b=sqrt(a);
+  printf("sqrt of a given value =%f",b);
+    return 0;
 }
